5b28c7ec6c1f8a0a285933f84b0f6aa26841b50a
Ravi Suhag <suhag.ravi@gmail.com>
2015-07-16 23:41:18 +0530

    Adding exec

---
+++ bin/ravisuhag.bin.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// --   use 'npm link' to test locally so that you dpnt need to reinstall over and again while building 
+
+console.log('+-+-+-+-+-+-+-+-+-+-+');
+console.log('|    Ravi Suhag     |');
+console.log('+-+-+-+-+ +-+-+-+-+-+');
+
+var userArgs = process.argv.slice(2);
+var subCommand = userArgs[0];
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.info = function() {
     console.log("Ravi Suhag is an award-winning web architect, designer, developer, hardware hacker and entrepreneur from India. He helps people build great products.");
 };
+
+
